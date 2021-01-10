1ea7e389f2096b5a985fc22542f3ca6c37b5a36a
johnnyyvard <johnnyyvard@me.com>
2021-01-10 16:31:38 +0100

    Implémentation du composant ToDoList

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,15 @@
-export default(state, action) => {
-    switch (action.type){
+export default (state, action) => {
+    switch (action.type) {
         case 'GET_TASKS':
-            return{
+            return {
                 ...state,
             }
 
-            case 'ADD_TASK':
-                return{
-                    ...state,
-                    tasks: [...state.tasks, action.task],
-                }
+        case 'ADD_TASK':
+            return {
+                ...state,
+                tasks: [...state.tasks, action.task],
+            }
 
         default:
             return state
